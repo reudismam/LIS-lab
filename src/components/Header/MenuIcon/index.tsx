9ce56fd6a667f571c25844bb321685efeb189399
reudismam <reudismam@gmail.com>
2020-08-30 18:35:04 -0300

    Hamburguer menu

---
+++ src/components/Header/MenuIcon/index.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+
+const MenuIcon = styled.div`
+    font-size: 2rem;
+    color: var(--branco);
+
+    @media (min-width: 768px) {
+        display: none;
+    }
+`
+
+export default MenuIcon;
