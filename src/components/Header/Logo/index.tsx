0c7f98f4b1b85f523fd915112d7cc37a6346259c
reudismam <reudismam@gmail.com>
2020-08-29 21:54:15 -0300

    Adicinando a logo.

---
+++ src/components/Header/Logo/index.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import logo from '../../../assets/images/lis.svg';
+
+const Logo = styled.img.attrs({src: logo, alt:"Logo do LIS"})`
+    height: 2rem;
+    margin-left: 1.6rem;
+`
+
+export default Logo;
