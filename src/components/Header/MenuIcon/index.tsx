bc3a7d1d8f9b93a12dad37a80e0bd07d1085b199
reudismam <reudismam@gmail.com>
2020-09-01 21:45:25 -0300

    media queries.

--- src/components/Header/MenuIcon/index.tsx
+++ src/components/Header/MenuIcon/index.tsx
@@ -3,6 +3,10 @@ import styled from 'styled-components';
 const MenuIcon = styled.div`
     font-size: 2rem;
     color: var(--branco);
+
+    @media (min-width: 768px) {
+        display: none;
+    }
 `
 
 export default MenuIcon;
