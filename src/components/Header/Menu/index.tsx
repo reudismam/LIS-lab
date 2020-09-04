fcdc20321b6e1f7b0c8912b6585be32b5e5b781f
reudismam <reudismam@gmail.com>
2020-09-04 14:12:14 -0300

    menu lateral.

--- src/components/Header/Menu/index.tsx
+++ src/components/Header/Menu/index.tsx
@@ -2,15 +2,36 @@ import React from 'react';
 import styled from 'styled-components';
 import {FiMenu} from 'react-icons/fi';
 import MenuIcon from '../MenuIcon';
+import MenuList from '../MenuList';
 
 const MenuContainer = styled.div`
     margin-left: 1.6rem;
     margin-right: 1.6rem;
+    display: flex;
+    flex: 1;
 
     & ${MenuIcon} {
         position: fixed;
         right: 20px;
         top: 10px;
+    }
+`
+
+const Menus = styled.nav`
+    background-color: var(--branco);
+    width: 200px;
+    height: 100vh;
+    position: fixed;
+    right: 0;
+    top: 0;
+
+    @media(min-width: 768px) {
+        background-color: transparent;
+        display: flex;
+        position: static;
+        height: 100%;
+        flex: 1;
+        justify-content: space-between;  
     }
 `
 
@@ -20,6 +41,26 @@ export default function Menu() {
             <MenuIcon>
                 <FiMenu />
             </MenuIcon>
+            
+            <Menus>
+                <MenuList>
+                    <li>Início</li>
+                    <li>Pesquisadores</li>
+                    <li>Publicações</li>
+                    <li>Projetos</li>
+                    <li>Portifólio</li>
+                    <li>Notícias</li>
+                    <li>Editais</li>
+                    <li>Parceiros</li>
+                    <li>Contato</li>
+                    <li>Sobre</li>
+                </MenuList>
+
+                <MenuList>
+                    <li>Sair</li>
+                    <li>Perfil</li>
+                </MenuList>
+            </Menus>
         </MenuContainer>
     );
 }
---
+++ src/components/Header/MenuList/index.tsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+
+const MenuList = styled.ul`
+    list-style: none;
+    margin-left: 3rem;
+    margin-top: 3rem;
+
+    li {
+        color: var(--cinza);
+        margin-bottom: 1rem;
+        font-size: 1.6rem;
+    }
+
+    @media(min-width: 768px) {
+        display: flex;
+        margin: 0;
+
+        li {
+            margin-left: 1rem;
+            margin-bottom: 0;
+            color: var(--branco);
+        }
+    }
+`
+
+export default MenuList;
