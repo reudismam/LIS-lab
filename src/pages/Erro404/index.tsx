c58b600bbbdf143ad85cb02b17f31d435cd216c6
reudismam <reudismam@gmail.com>
2020-09-07 14:11:52 -0300

    line

--- src/pages/Erro404/index.tsx
+++ src/pages/Erro404/index.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import PageTemplate from '../PageTemplate';
-import error404Img from '../../assets/images/Erro404/erro404.svg';
+import error404Img from '../../assets/images/Error404/erro404.svg';
 
 export default function Erro404() {
     return (
         <PageTemplate
             imageSrc={error404Img}
-            imageAlt="Erro 404"
+            imageAlt="Error 404"
             title="Erro 404"
             description="Página não encontrada!"
         >
             <h1>Erro 404</h1>
+            
         </PageTemplate>
     );
 }
