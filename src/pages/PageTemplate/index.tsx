4b9742cfb028def8ec2e20bc3d171d8d306b0a34
reudismam <reudismam@gmail.com>
2020-09-11 13:48:33 -0300

    Configurando o corpo da aplicação.

--- src/pages/PageTemplate/index.tsx
+++ src/pages/PageTemplate/index.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import Header from '../../components/Header/Container';
 import Footer from '../../components/Footer/Container';
 import {BannerProps} from '../../components/Header/Banner';
+import Container from '../../components/Container';
 
 const PageTemplate: React.FC<BannerProps> = (props) => {
     return (
         <>
             <Header {...props}/>
-            {props.children}
+            <Container>
+                {props.children}
+            </Container>
             <Footer />
         </>
     );
