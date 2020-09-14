5563d40347edc71fe6e3d5f3f7169e3b31614281
reudismam <reudismam@gmail.com>
2020-09-14 20:32:41 -0300

    projetos para tela grande.

--- src/components/ProjetosCard/index.tsx
+++ src/components/ProjetosCard/index.tsx
@@ -3,6 +3,10 @@ import styled from 'styled-components';
 const Card = styled.article`
     height: 600px;
     overflow: hidden;
+
+    @media(min-width: 768px) {
+        width: 48%;
+    }
 `
 
 export default Card;
