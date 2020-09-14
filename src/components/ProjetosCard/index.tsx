1af61be4303b9b8dbb912f1d8cb7ebafc341d4c8
reudismam <reudismam@gmail.com>
2020-09-14 20:55:50 -0300

    tela mobile.

--- src/components/ProjetosCard/index.tsx
+++ src/components/ProjetosCard/index.tsx
@@ -3,10 +3,6 @@ import styled from 'styled-components';
 const Card = styled.article`
     height: 600px;
     overflow: hidden;
-
-    @media(min-width: 768px) {
-        width: 48%;
-    }
 `
 
 export default Card;
