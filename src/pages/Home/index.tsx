af9d5118b1380e746b59d04219cbacb912d3ef47
reudismam <reudismam@gmail.com>
2020-09-15 14:29:19 -0300

    Adicionando vídeo destaque

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PageTemplate from '../PageTemplate';
 import homeImg from '../../assets/images/Home/inicio.svg';
 import Projetos from '../../components/Projetos';
+import VideoDestaque from '../../components/VideoDestaque';
 
 export default function Home() {
     return (
@@ -13,6 +14,7 @@ export default function Home() {
             buttonText="Projetos"
         >
             <Projetos />
+            <VideoDestaque />
         </PageTemplate>
     );
 }
