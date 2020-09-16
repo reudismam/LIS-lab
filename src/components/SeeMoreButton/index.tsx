a9147803027b5c65d4597423ac130875b199be49
reudismam <reudismam@gmail.com>
2020-09-16 20:41:15 -0300

    Concluindo a tela home.

---
+++ src/components/SeeMoreButton/index.tsx
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const SeeMoreButton = styled(Link)`
+    margin: 10px auto;
+    display: flex;
+    background-color: var(--primaria);
+    width: 90%;
+    height: 6rem;
+    border: 1px solid var(--cinza);
+    justify-content: center;
+    align-items: center;
+    color: var(--branco);
+    text-decoration: none;
+    font-family: 'Roboto', sans-serif;
+    font-size: 1.4rem;
+    border-radius: 0.8rem;
+    margin-bottom: 20px;
+
+    @media(min-width: 768px) {
+        margin-right: 0px;
+        width: 200px;
+    }
+`
+
+export default SeeMoreButton;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,6 +3,18 @@ import PageTemplate from '../PageTemplate';
 import homeImg from '../../assets/images/Home/inicio.svg';
 import Projetos from '../../components/Projetos';
 import VideoDestaque from '../../components/VideoDestaque';
+import SeeMoreButton from '../../components/SeeMoreButton';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+    color: var(--fundo);
+    width: 100%;
+    text-align: center;
+    margin: 40px 0;
+    font-family: 'Roboto', sans-serif;
+    font-size: 2.4rem;
+`
+
 
 export default function Home() {
     return (
@@ -13,8 +25,16 @@ export default function Home() {
             buttonLink="/projetos"
             buttonText="Projetos"
         >
+            <Title>
+                Confira os nossos projetos recentes
+            </Title>
             <Projetos />
+            <SeeMoreButton to="./projetos">Mais projetos</SeeMoreButton>
+            <Title>
+                Vídeo em destaque
+            </Title>
             <VideoDestaque />
+            <SeeMoreButton to="./videos">Mais vídeos</SeeMoreButton>
         </PageTemplate>
     );
 }
