4497076d3b5a156edbd079f8ae886d7773e62665
reudismam@gmail.com <reudismam@gmail.com>
2020-11-04 09:41:40 -0300

    handle events

--- src/pages/CriarConta/index.tsx
+++ src/pages/CriarConta/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {FormEvent, useState} from 'react';
 import styled from 'styled-components';
 import PageTemplate from '../PageTemplate';
 import criarContaImg from '../../assets/images/CriarConta/criarConta.svg';
@@ -88,15 +88,17 @@ const Property = styled.div`
         display: flex;
         justify-content: space-between;
         width: 100%;
+
+        ${SelectContainer} {
+            width: 100%;
+        }
+
+        ${SelectContainer} + ${SelectContainer} {
+            margin-left: 10px;
+        }
     }
 
-    ${SelectContainer} {
-        width: 100%;
-    }
-
-    ${SelectContainer} + ${SelectContainer} {
-        margin-left: 10px;
-    }
+    
 `
 
 const TopFieldSet = styled.div`
@@ -113,10 +115,93 @@ const PlusButton = styled.button`
 `
 
 const TitleContainer = styled.div`
+     @media(min-width: 768px) {
+        display: flex;
+        width: 100%;
 
+        ${InputContainer} {
+            width: 100%;
+        }
+
+        ${InputContainer} + ${InputContainer} {
+            width: 30%;
+            margin-left: 10px;
+        }
+    }
 `
 
+const PublicationContainer = styled.div`
+`
+
+interface Publication {
+    title: string,
+    year: string,
+    reference: string
+}
+
 export default function CriarConta() {
+    const [name, setName] = useState('');
+    const [lastname, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [course, setCourse] = useState('');
+    const [category, setCategory] = useState('');
+    const [occupation, setOccupation] = useState('');
+    const [degree, setDegree] = useState('');
+    const [bio, setBio] = useState('');
+    const [areas, setAreas] = useState<string[]>(['']);
+    const [publications, setPublications] = useState<Publication[]>([{
+        title: '',
+        year: '',
+        reference: ''
+    }]);
+
+    function setAreaInteresse(position: number, value: string) {
+        const areasAtualizadas = areas.map((area, index) => {
+            if (index === position) {
+                return value;
+            }
+            else {
+                return area;
+            }
+        })
+        setAreas(areasAtualizadas);
+    }
+
+    function setPublication(position: number, property: string, value: string) {
+        const publicationsAtualizadas = publications.map((publication, index) => {
+            if (position === index) {
+                return (
+                    {
+                        ...publication,
+                        [property]: value
+                    }
+                );
+            }
+            else {
+                return publication;
+            }
+        })
+        setPublications(publicationsAtualizadas);
+    }
+
+    function handleSubmit(e: FormEvent) {
+            e.preventDefault();
+            console.log({
+                name, 
+                lastname,
+                email,
+                phone,
+                course,
+                category,
+                occupation,
+                degree,
+                bio,
+                areas,
+                publications
+            });
+    }
+
     return (
         <PageTemplate
             imageSrc={criarContaImg}
@@ -124,7 +209,7 @@ export default function CriarConta() {
             title="Quase tudo pronto para se tornar um membro do grupo"
             description="Insira seus dados cadastrais nos campos abaixo."
         >
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <FieldSet>
                     <Legend>
                         Seus Dados
@@ -135,8 +220,18 @@ export default function CriarConta() {
                         </Photo>
 
                         <Name>
-                            <Input type="text" name="name" label="Nome"/>
-                            <Input type="text" name="lastname" label="Sobrenome"/>
+                            <Input 
+                                type="text" 
+                                name="name" 
+                                label="Nome"
+                                onChange={e => setName(e.target.value)}
+                                />
+                            <Input 
+                                type="text" 
+                                name="lastname" 
+                                label="Sobrenome"
+                                onChange={e => setLastName(e.target.value)}
+                                />
                         </Name>
                     </NameContainer>
                     <Contato>
@@ -145,33 +240,47 @@ export default function CriarConta() {
                                 name="email" 
                                 label="Email"
                                 placeholder="example@email.com"
+                                onChange={e => setEmail(e.target.value)}
                                 />
                             <Input 
                                 type="number" 
                                 name="phone" 
                                 label="Telefone"
                                 placeholder="(__)  9 ____  ____"
+                                onChange={e => setPhone(e.target.value)}
                                 />
                     </Contato>
-                    <Input type="text" name="course" label="Curso" />
+                    <Input 
+                        type="text" 
+                        name="course" 
+                        label="Curso" 
+                        onChange={e => setCourse(e.target.value)}
+                        />
                     <Property>
                         <Select
                             name="category"
                             label="Categoria (discente, docente)"
                             options={categoria}
+                            onChange={e => setCategory(e.target.value)}
                             />
                         <Select 
                             name="occupation"
                             label="Função (Membro, Líder)"
                             options={funcao}
+                            onChange={e => setOccupation(e.target.value)}
                             />
                         <Select
                             name="degree"
                             label="Titulação (graduando, mestre)"
                             options={titulacao} 
+                            onChange = {e => setDegree(e.target.value)}
                         />
                     </Property>
-                    <TextArea name="bio" label=" Bio (max  300 caracteres)"/>
+                    <TextArea 
+                        name="bio" 
+                        label=" Bio (max  300 caracteres)"
+                        onChange={e => setBio(e.target.value)}
+                        />
                 </FieldSet>
                 <FieldSet>
                     <TopFieldSet>
@@ -180,7 +289,17 @@ export default function CriarConta() {
                         </Legend>
                         <PlusButton>+Área</PlusButton>
                     </TopFieldSet>
-                    <Input name="area" label="Área de Interesse"/>
+                    {
+                        areas.map((area, index) => {
+                            return (
+                                <Input key={`Area ${index}`}
+                                    name="area" 
+                                    label="Área de Interesse"
+                                    onChange={e => setAreaInteresse(index, e.target.value)}
+                                    />
+                            )
+                        })        
+                    }
                 </FieldSet>
                 <FieldSet>
                     <TopFieldSet>
@@ -189,13 +308,33 @@ export default function CriarConta() {
                         </Legend>
                         <PlusButton>+Publicações</PlusButton>
                     </TopFieldSet>
-                    <TitleContainer>
-                        <Input name="publication_tile" label="Título"/>
-                        <Input name="publication_year" label="Ano" />                    
-                    </TitleContainer>
-                    <TextArea name="publication_reference" label="Referência ABNT (max. 300 caracteres)"/>
-                </FieldSet>
-                <SubmitButton>Enviar</SubmitButton>
+                    {
+                        publications.map((publication, index) => {
+                            return (
+                                <PublicationContainer key={`Publication ${index}`}>
+                                    <TitleContainer>
+                                        <Input 
+                                            name="publication_title" 
+                                            label="Título"
+                                            onChange={e => setPublication(index, "title", e.target.value)}
+                                            />
+                                        <Input 
+                                            name="publication_year" 
+                                            label="Ano" 
+                                            onChange={e => setPublication(index, "year", e.target.value)}
+                                            />                    
+                                    </TitleContainer>
+                                    <TextArea 
+                                        name="publication_reference" 
+                                        label="Referência ABNT (max. 300 caracteres)"
+                                        onChange={e => setPublication(index, "reference", e.target.value)}
+                                        />
+                                </PublicationContainer>
+                            );
+                        })
+                    }
+                    </FieldSet>
+                <SubmitButton type="submit">Enviar</SubmitButton>
             </Form>
         </PageTemplate>
     );
